e1907e215151791565d75bb6e5d032102addf2cd
max <max.nick.demax@gmail.com>
2022-05-02 10:16:23 +0300

    initial commit

---
+++ src/test/java/com/github/maksimdenisov/menuplanner/MenuPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.maksimdenisov.menuplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
